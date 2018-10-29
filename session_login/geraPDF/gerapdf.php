bef04338eb9c8a1f0b2cd88db5e143df874aa20f
raislan <raislan@live.com>
2018-10-29 18:45:51 -0300

    commuit

--- session_login/geraPDF/gerapdf.php
+++ session_login/geraPDF/gerapdf.php
@@ -1,4 +1,12 @@
 <?php
+
+$nome = "raislan";
+$sobrenome = "da silva";
+$atendimento = "Concluido";
+$cargo = "Desenvolvedor";
+
+
+
 /* Carrega a classe DOMPdf */
 require_once("dompdf/dompdf_config.inc.php");
 
@@ -6,7 +14,21 @@ require_once("dompdf/dompdf_config.inc.php");
 $dompdf = new DOMPDF();
 
 /* Carrega seu HTML */
-$dompdf->load_html('<p>Adicione seu HTML aqui.</p>');
+$dompdf->load_html('
+
+  <p style="font-size:13px; font-weight: bold;">Assinatura do Solictante:_________________________________________________________________________________</p>
+
+  <p style="font-size:13px; font-weight: bold;">Data Fechamento:_______________________________________________________________________________________</p>
+
+  <form style="font-size:13px; font-weight: bold;" action="/action_page.php">
+      <label>Autorizo fechar o chamado:</label>
+      <input style="width: 14px;" type="checkbox" name="vehicle1" value="Bike"> sim
+      <input style="width: 14px;" type="checkbox" name="vehicle2" value="Car"> não porque?<labe>___________________________________________________________</label>
+      <hr style="color:white; width:0.5px;"> 
+      <label>_______________________________________________________________________________________________________ </label>
+  </form>
+
+');
 
 /* Renderiza */
 $dompdf->render();
